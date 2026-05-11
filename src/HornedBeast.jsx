1c9187693aadbfdde00756a8287751abad5c6ddf
Naomi Torres <ivanys.ntm@gmail.com>
2026-05-11 14:22:43 -0700

    finished feature1

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,6 +1,4 @@
 
-// LAB 01 - FEATURE TASKS
-// The HornedBeast component needs to contain an <h2> that displays the title of the animal, an <img> element with src, alt and title attributes, and a <p> that displays the description.
 
 function HornedBeast(props) {
 
