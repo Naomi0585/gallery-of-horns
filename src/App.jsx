15a3e62da41505691e90cb439be0c81305284704
Naomi Torres <ivanys.ntm@gmail.com>
2026-05-11 11:24:50 -0700

    header typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Header from './components/Header';
-import Gallery from './components/Gallery';
-import Footer from './components/Footer';
+import './App.css';
+import Header from './header';
+import Gallery from './Gallery';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div>
+    <div className="App">
 
       <Header />
 
